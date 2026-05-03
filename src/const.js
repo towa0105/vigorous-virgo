fa33f582b506b58c4facea396095e1ebb34eca57
towa0105 <2250290@ecc.ac.jp>
2026-05-04 00:25:02 +0900

    完了：環境構築

---
+++ src/const.js
@@ -0,0 +1,5 @@
+export const lang = "ja";
+export const title = "農業IOT";
+export const description = "農業IoT の学習と制作を進めるための Astro サイトです。";
+export const x_name = "のうぎょう";
+export const url = "https://comp.ecc.ac.jp/";
